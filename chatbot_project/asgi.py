1969436d166c6a3a6235f6fcddb31ba5b167f95d
Vetri <vetri1998kpr@gmail.com>
2025-07-03 00:40:03 +0530

    Fix WebSocket protocol for production

--- chatbot_project/asgi.py
+++ chatbot_project/asgi.py
@@ -1,17 +1,8 @@
-"""
-ASGI config for chatbot_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information, see:
-https://channels.readthedocs.io/en/stable/
-"""
-
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing  # make sure this file exists
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # <-- this must match your app name
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot_project.settings')
 
--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -74,6 +74,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chatbot_project.wsgi.application'
 
+ASGI_APPLICATION = 'chatbot_project.asgi.application'
+
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
